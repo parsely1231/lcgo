9b2dff8245877eb78fe696b2b6248dcb70cc3808
parsely <parsely_1231@icloud.com>
2022-02-11 19:12:42 +0900

    implement chromatgram

--- src/domain/chromatogram.go
+++ src/domain/chromatogram.go
@@ -1,12 +1,26 @@
 package domain
 
+import (
+	"fmt"
+)
+
 // Chromatogram is a chromatgram output from HPLC
 type Chromatogram struct {
-	Name  string
-	peeks []Peek
+	Name     string
+	peeksMap map[int]*Peek
 }
 
 // AddPeek add peek to chromatogram
-func (c *Chromatogram) AddPeek(peek Peek) {
-	c.peeks = append(c.peeks, peek)
+func (c *Chromatogram) AddPeek(peek *Peek) {
+	id := len(c.peeksMap) + 1
+	c.peeksMap[id] = peek
+}
+
+// FindPeek return a peek, or error if id does not exist.
+func (c *Chromatogram) FindPeek(id int) (*Peek, error) {
+	peek, exist := c.peeksMap[id]
+	if !exist {
+		return nil, fmt.Errorf(`id: %v does not exist`, id)
+	}
+	return peek, nil
 }
